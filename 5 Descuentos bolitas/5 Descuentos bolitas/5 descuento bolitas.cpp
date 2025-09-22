df92d7cacb21e9c8c35f609e08ffb1061207c1c5
Izan Gutierrez <81985413+XxGodIzanxX@users.noreply.github.com>
2025-09-22 16:12:16 +0200

    Ejercicio 5 (deberes 24/09/2025)

--- 5 Descuentos bolitas/5 Descuentos bolitas/5 descuento bolitas.cpp
+++ 5 Descuentos bolitas/5 Descuentos bolitas/5 descuento bolitas.cpp
@@ -7,7 +7,7 @@ int main() {
     printf("Ingrese el valor total de la compra: ");
     scanf_s("%f", &total);
 
-    printf("Ingrese el color de la bolita (1=blanco, 2=negro, 3=azul): ");
+    printf("Ingrese el color de la bolita (1=blanco, 2=negro, 3=azul, 4=rosa, 5=dorado): ");
     scanf_s("%d", &bolita);
 
     switch (bolita) {
@@ -19,6 +19,12 @@ int main() {
         break;
     case 3: // Azul
         final = total * 0.75; // 25% de descuento
+        break;
+    case 4: // rosa
+        final = total * 1; // 25% de descuento
+        break;
+    case 5: // dorado
+        final = total * 1.25; // 25% de descuento
         break;
     default:
         printf("Color de bolita no valido.\n");

d66ce0959e549e4dcba576906a43531fb180bc23
Izan Gutierrez <81985413+XxGodIzanxX@users.noreply.github.com>
2025-09-24 17:03:04 +0200

    Ejercicio 2 (deberes 29/09/2025)

---
+++ Ejercicio2 29-09-2025/Ejercicio2 29-09-2025/Ejercicio2 29-09-2025.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+
+	int i = 0;
+
+	while (i <= 100)
+
+	{
+				if (i % 5 == 0)
+									{
+					printf("El numero %d es multiplo de 5\n", i);
+				}
+				i++;
+	}
+}

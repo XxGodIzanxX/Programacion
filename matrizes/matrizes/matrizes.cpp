7dd26a1462a0a416fcd81c05c7a093be7b713198
Izan Gutierrez <81985413+XxGodIzanxX@users.noreply.github.com>
2025-10-06 16:02:30 +0200

    Ejercicios de clase

---
+++ matrizes/matrizes/matrizes.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define SIZEMATRIX 4
+int main() {
+	/*
+	int v1[10][10];
+
+	for (int i = 0; i <= 10; i++)
+	{
+		for (int j = 0; j <= 10; j++)
+		{
+			printf(" - ");
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	int v1[SIZEMATRIX][SIZEMATRIX];
+
+	for (int i = 0; i <= SIZEMATRIX; i++)
+	{
+		for (int j = 0; j <= SIZEMATRIX; j++)
+		{
+			scanf_s("%d", &v1[i][j]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i <= SIZEMATRIX; i++)
+	{
+		for (int j = 0; j <= SIZEMATRIX; j++)
+		{
+			v1[i][j] *= 2;
+			printf(" %d ", v1[i][j]);
+		}
+		printf("\n";);[]
+	}
+	*/
+
+	int v1[4][4];
+
+	for (int i = 0; i <= 4; i++)
+	{
+
+	}
+
+
+
+}

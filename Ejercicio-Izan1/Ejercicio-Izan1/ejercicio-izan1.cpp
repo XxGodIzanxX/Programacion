73e0e0edf374bd126eb5a483b65674cb8ab1a817
Izan Gutierrez <81985413+XxGodIzanxX@users.noreply.github.com>
2025-10-15 16:19:15 +0200

    Ejercicio W3

---
+++ Ejercicio-Izan1/Ejercicio-Izan1/ejercicio-izan1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+	/*
+		int profe = 1;
+		int alumno = 2;
+		int director = 3;
+		int rol;
+
+		rol = profe * alumno - director;
+		printf("El profe es %d", )
+		printf("Numero= %d", rol);
+
+		*/
+
+	int age = 18;
+
+	printf("%d\n", age >= 18);
+	printf("%d\n", age < 18);
+	
+
+}

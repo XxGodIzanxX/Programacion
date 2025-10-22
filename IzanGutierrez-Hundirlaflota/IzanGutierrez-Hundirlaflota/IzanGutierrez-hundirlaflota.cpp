24cc9c5f5d5452b59cd0470660be50a8d086de9b
Izan Gutierrez <81985413+XxGodIzanxX@users.noreply.github.com>
2025-10-22 13:11:51 +0200

    Actividad 6 (deberes 22/10/2025)

--- IzanGutierrez-Hundirlaflota/IzanGutierrez-Hundirlaflota/IzanGutierrez-hundirlaflota.cpp
+++ IzanGutierrez-Hundirlaflota/IzanGutierrez-Hundirlaflota/IzanGutierrez-hundirlaflota.cpp
@@ -2,88 +2,63 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 10
-#define NUM_NAVES 3
-#define LONG_NAVE 3
-
 int main() {
-    srand(time(NULL));
+    char tablero[10][10];
+    int i, j, x, y, aciertos = 0, intentos = 0;
 
-    char mapa[SIZE][SIZE];
-    int naves[SIZE][SIZE];
-    int direcciones[4][2] = { {1,0},{0,1},{1,1},{-1,1} };
-    int aciertos = 0, intentos = 0;
+    
+    for (i = 0; i < 10; i++)
+        for (j = 0; j < 10; j++)
+            tablero[i][j] = '.';
 
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++) {
-            mapa[i][j] = ' ';
-            naves[i][j] = 0;
-        }
-    int colocadas = 0;
-    while (colocadas < NUM_NAVES) {
-        int x = rand() % SIZE;
-        int y = rand() % SIZE;
-        int d = rand() % 4;
-        int dx = direcciones[d][0];
-        int dy = direcciones[d][1];
-        int valido = 1;
-        for (int i = 0; i < LONG_NAVE; i++) {
-            int nx = x + i * dx;
-            int ny = y + i * dy;
-            if (nx < 0 || nx >= SIZE || ny < 0 || ny >= SIZE || naves[ny][nx] == 1) {
-                valido = 0;
-                i = LONG_NAVE;
-            }
-        }
-        if (valido) {
-            for (int i = 0; i < LONG_NAVE; i++) {
-                int nx = x + i * dx;
-                int ny = y + i * dy;
-                naves[ny][nx] = 1;
-            }
-            colocadas++;
-        }
-    }
-    while (aciertos < NUM_NAVES * LONG_NAVE) {
-  
+   
+    srand((unsigned)time(NULL));
+    int fila = rand() % 10;
+    int columna = rand() % 8; 
+    for (i = 0; i < 3; i++)
+        tablero[fila][columna + i] = 'S';
+
+    
+    while (aciertos < 3) {
+       
         printf("  ");
-        for (int i = 0; i < SIZE; i++) printf("%d ", i);
+        for (i = 0; i < 10; i++) printf("%d ", i);
         printf("\n");
-        for (int i = 0; i < SIZE; i++) {
+        for (i = 0; i < 10; i++) {
             printf("%d ", i);
-            for (int j = 0; j < SIZE; j++) {
-                printf("%c ", mapa[i][j]);
+            for (j = 0; j < 10; j++) {
+                if (tablero[i][j] == 'S' || tablero[i][j] == '.')
+                    printf(". ");
+                else
+                    printf("%c ", tablero[i][j]);
             }
             printf("\n");
         }
-        int x, y;
-        printf("Introduce coordenadas x y (0-9): ");
+
+        
+        printf("Dispara (fila columna): ");
         scanf_s("%d %d", &x, &y);
 
-        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
-            printf("Coordenadas fuera de rango.\n");
+        if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+            printf("Fuera del tablero.\n");
+            continue;
         }
-        else if (mapa[y][x] != ' ') {
-            printf("Ya has disparado aquí.\n");
+
+        if (tablero[x][y] == 'S') {
+            printf("¡Tocado!\n");
+            tablero[x][y] = 'X';
+            aciertos++;
+        }
+        else if (tablero[x][y] == '.') {
+            printf("Agua.\n");
+            tablero[x][y] = 'O';
         }
         else {
-            intentos++;
-            if (naves[y][x] == 1) {
-                mapa[y][x] = 'X';
-                aciertos++;
-                printf("¡Impacto!\n");
-            }
-            else {
-                mapa[y][x] = 'O';
-                printf("Agua...\n");
-            }
+            printf("Ya disparaste ahí.\n");
         }
+
+        intentos++;
     }
-    printf("\n¡Has destruido todas las naves en %d intentos!\n", intentos);
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            printf("%c ", mapa[i][j]);
-        }
-        printf("\n");
-    }
+
+    printf("¡Hundiste la nave en %d intentos!\n", intentos);
 }
